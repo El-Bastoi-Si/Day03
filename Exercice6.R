bcf2779a41b6bcb3195fb10d298313b5b7015a88
El-Bastoi-Si <elbastois@outlook.fr>
2025-01-08 12:11:36 +0100

    Create Exercice6.R

---
+++ Exercice6.R
@@ -0,0 +1,6 @@
+Nom = c("Alice", "Bob", "Clara", "David", "Emma", "Fred", "Gina")
+Age = c(22, 25, 20, 23, 24, 26, 21)
+Note= c(15, 18, 14, 16, 17, 19, 20)
+etudiant = data.frame(Nom, Age ,Note)
+print(etudiant[,c("Nom","Age")])
+F = filter(etudiant,Note>=15)
